bc57368201860caf1e425cb3f4e08d480fc4213b
Rodrigo Carbonel <rcarbonel4471@gmail.com>
2024-09-23 02:19:25 -0500

    Carga inicial

---
+++ src/main/java/com/edu/cibertec/examen_backend_b/ExamenBackendBApplication.java
@@ -0,0 +1,13 @@
+package com.edu.cibertec.examen_backend_b;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamenBackendBApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamenBackendBApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/edu/cibertec/examen_backend_b/ExamenBackendBApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edu.cibertec.examen_backend_b;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamenBackendBApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
